23d155b613bfd09e969862284e7aace056014b6b
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-07-12 22:40:56 -0300

    add mailer

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
 
   def dispatch_mail
     respond_to do |format|
+      VisitorMailer.contact_mail(params[:name], params[:email], params[:message]).deliver_now
       format.html { redirect_to :root }
     end
   end
