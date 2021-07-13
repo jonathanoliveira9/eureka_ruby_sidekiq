b07c44897f98817d79a8d8d13621ed70599e4942
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-07-12 21:51:04 -0300

    update view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
   def index; end
 
   def dispatch_mail
-    
+    respond_to do |format|
+      format.html { redirect_to :root }
+    end
   end
 end
