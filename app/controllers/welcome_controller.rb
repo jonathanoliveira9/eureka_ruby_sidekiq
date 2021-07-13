f7fad46d0332d5ead644fb76bac7a0c7a170955c
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-07-12 21:35:35 -0300

    install boostrap

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,7 @@
 class WelcomeController < ApplicationController
   def index; end
+
+  def dispatch_mail
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: "welcome#index"
+  resources :welcome do
+    collection do
+      get  :dispatch_mail
+    end
+  end
 end
