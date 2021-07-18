d9198f07a695e3fcd6fc2b99389bb87dd8b03e46
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-07-17 23:19:23 -0300

    remove constant unnecessary

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  REGEX_MAIL = 
   validates :first_name, :last_name, :email, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP },
                     length: { minimum: 4, maximum: 254 },
