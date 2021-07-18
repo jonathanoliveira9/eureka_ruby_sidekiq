bbadcfab06d7ebce37d8d025db235724cfdc8bae
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-07-17 23:18:48 -0300

    add user

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  REGEX_MAIL = 
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP },
+                    length: { minimum: 4, maximum: 254 },
+                    uniqueness: { case_sensitive: false }
+end
