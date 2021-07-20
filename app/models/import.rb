bcbf5c60f4841e50184696954c530017fd687ca2
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-07-19 21:10:24 -0300

    add generate code

--- app/models/import.rb
+++ app/models/import.rb
@@ -2,4 +2,10 @@ class Import < ApplicationRecord
   has_many :import_lines
   enum status: [:fail, :in_progress, :success]
   validates :status, presence: true
+  after_create :generate_code
+
+  def generate_code
+    self.code = Date.today.strftime("%Y%m%d") + id.to_s
+    self.save
+  end
 end
