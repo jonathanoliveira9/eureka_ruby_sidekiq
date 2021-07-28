6e778d4f9e9cba54017553e64d9822ba2475a5cb
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-07-27 23:03:29 -0300

    add file_template

--- app/models/import.rb
+++ app/models/import.rb
@@ -1,8 +1,10 @@
 class Import < ApplicationRecord
   has_many :import_lines, dependent: :destroy
-  enum status: [:fail, :in_progress, :success]
+  enum status: [:failed, :in_progress, :success]
   validates :status, presence: true
   after_create :generate_code
+
+  has_one_attached :attachment_template
 
   def generate_code
     self.code = Date.today.strftime("%Y%m%d") + id.to_s
