5e16f273bf9dd019d8c523c3170aa0b23e15fdb2
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-08-05 22:35:52 -0300

    update gem spreadsheet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_022038) do
+ActiveRecord::Schema.define(version: 2021_08_03_005333) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,10 @@ ActiveRecord::Schema.define(version: 2021_07_27_022038) do
     t.string "string"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
   end
 
   create_table "imports", charset: "utf8mb4", force: :cascade do |t|
@@ -62,6 +66,7 @@ ActiveRecord::Schema.define(version: 2021_07_27_022038) do
     t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "type"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
