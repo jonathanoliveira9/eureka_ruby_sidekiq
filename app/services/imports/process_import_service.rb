a4d3eee3850f4601162f22d1fd1730dfb296793c
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-08-07 15:28:14 -0300

    process bulk users

---
+++ app/services/imports/process_import_service.rb
@@ -0,0 +1,40 @@
+module Imports
+  class ProcessImportService
+    attr_accessor :import, :errors
+
+    def self.run(import_id)
+      new(import_id).process
+    end
+
+    def initialize(import_id)
+      @import = Import.find(import_id)
+      @errors = []
+    end
+
+    def process
+      create_lines
+      import.success!
+    rescue => e
+      import.failed!
+    end
+
+    private
+
+    def fetch_rows
+      Imports::FetchRowsService.new(import: import).process
+    end
+
+    def header_file
+      { first_name: 'First_Name', last_name: 'Last_Name', email: 'Email', phone: 'Phone' }
+    end
+
+    def create_lines
+      fetch_rows.each(header_file) do |line|
+
+        next if line.each_value.all? { |value| header_file.values.include?(value) }
+
+        ImportLines::CreateLinesService.new(line, import.id).call
+      end
+    end
+  end
+end
