a7dbfc023a514086ddfd81bd4311501de943c74c
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-08-08 17:59:45 -0300

    update form

--- app/controllers/import_lines_controller.rb
+++ app/controllers/import_lines_controller.rb
@@ -1,5 +1,5 @@
 class ImportLinesController < ApplicationController
-  before_action :import_line, only: %i[edit]
+  before_action :find_import_line, only: %i[edit]
 
   def index
     @import_lines = ImportLine.by_import(params[:import_id])
@@ -7,15 +7,26 @@ class ImportLinesController < ApplicationController
 
   def edit; end
 
-  def update; end
+  def update
+    @import_line = ImportLine.find(params[:id])
+    @import_line.assign_attributes(params_import_lines)
+    respond_to do |format|
+      if @import_line.save
+        format.html
+        redirect_to import_import_lines_path(@import_line.import)
+      else
+        render :edit
+      end
+    end
+  end
 
   private
 
-  def import_line
+  def find_import_line
     @import_line = ImportLine.find(params[:id])
   end
 
   def params_import_lines
-    params.require(:import_lines).permit(:last_name, :first_name, :email, :phone, :id)
+    params.require(:import_line).permit(:last_name, :first_name, :email, :phone)
   end
 end
