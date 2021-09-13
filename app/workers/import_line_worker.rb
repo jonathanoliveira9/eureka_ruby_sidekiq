cacf35f241e92583f6448759dcc160d07116cdc6
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-09-13 13:11:51 -0300

    fix identation

--- app/workers/import_line_worker.rb
+++ app/workers/import_line_worker.rb
@@ -1,9 +1,8 @@
 class ImportLineWorker
-    include Sidekiq::Worker
-    sidekiq_options queue: 'bulk_users', retry: false
-  
-    def perform(import_line_id)
-      ImportLines::ProcessLineService.run(import_line_id)
-    end
+  include Sidekiq::Worker
+  sidekiq_options queue: 'bulk_users', retry: false
+
+  def perform(import_line_id)
+    ImportLines::ProcessLineService.run(import_line_id)
   end
-  
+end
