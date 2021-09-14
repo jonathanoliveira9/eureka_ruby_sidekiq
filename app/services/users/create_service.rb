5b83c084910aba2ecbc0146e498ecf2b756fcc90
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-09-13 23:18:22 -0300

    add create user factory

--- app/services/users/create_service.rb
+++ app/services/users/create_service.rb
@@ -13,13 +13,13 @@ module Users
     private
 
     def process
-      User.create(user_params)
+      User.create!(user_params)
     rescue ActiveModel::ValidationError => e
       raise e
     end
 
     def user_params
-      params.permit(:first_name, :last_name, :email, :phone)
+      ActionController::Parameters.new(params).permit(:first_name, :last_name, :email, :phone)
     end
   end
 end
